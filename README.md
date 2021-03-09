b33e3bc525b57ff1b7e9854a9bef89cb16930dae
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2021-03-09 09:43:42 +0900

    Update options

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 ## How to use
 
 ### Set environment variables
-- `JUPYTER_HUB_HOST`: Host and port (default: `127.0.0.1:8000`)
+- `EXPOSED_PORT`: Port where the metrics will be exposed (default: `9000`)
+- `METRICS_PREFIX`: Prefix to be added into the metrics names (default: `jue`)
+- `JUPYTER_HUB_HOST`: Host (default: `127.0.0.1`)
+- `JUPYTER_HUB_PORT`:  Port (default: `8000`)
 - `JUPYTER_HUB_API_TOKEN`: API token of JupyterHub (can be created on the JupyterHub Admin UI)
 - `JUE_INTERVAL`: Time interval to retrieve information (unit: seconds, default: 10)
 
 ### To execute directly 
 
-```python jupyterhub_users_exporter.py -p 8070```
+```python jupyterhub_users_exporter.py```
 
 ### By docker 
 
--- jupyterhub_users_exporter.py
+++ jupyterhub_users_exporter.py
@@ -7,13 +7,9 @@ from prometheus_client import Counter, start_http_server, Gauge
 
 metric_labels = ['user', 'org']
 
-api_host = 'http://' + os.environ.get('JUPYTER_HUB_HOST', '127.0.0.1:8000')
-api_url = api_host + '/hub/api'
-token = os.environ.get('JUPYTER_HUB_API_TOKEN', '')
-request_headers = {'Authorization': 'token %s' % token, }
-interval = int(os.environ.get('JUE_INTERVAL', 10))
-
-def monitor_metrics():
+def monitor_metrics(api_host, token, interval):
+    api_url = api_host + '/hub/api'
+    request_headers = {'Authorization': 'token %s' % token, }
     while True:
         try:
             r = requests.get(api_url + '/users',
@@ -58,16 +54,20 @@ def monitor_metrics():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', '-p', default=int(os.getenv('NOE_PORT', 8000)),
+    parser.add_argument('--port', '-p', default=int(os.getenv('EXPOSED_PORT', 9000)),
         help='The Prometheus metrics port.')
-    parser.add_argument('--metric_prefix', '-s', default=os.getenv('NOE_METRIC_PREFIX', 'jue'),
-        help='Metric prefix (group) for Prometheus')
+    parser.add_argument('--interval', '-i', default=int(os.environ.get('JUE_INTERVAL', 10)))
+    parser.add_argument('--metrics_prefix', '-m', default=os.getenv('METRICS_PREFIX', 'jue'),
+        help='Metrics prefix (group) for Prometheus')
+    parser.add_argument('--jupyterhub_host', default=os.environ.get('JUPYTER_HUB_HOST', '127.0.0.1'))
+    parser.add_argument('--jupyterhub_port', default=os.environ.get('JUPYTER_HUB_PORT', '8000'))
+    parser.add_argument('--jupyterhub_token', default=os.environ.get('JUPYTER_HUB_API_TOKEN', ''))
     opts = parser.parse_args()
 
-    server_num_gauge = Gauge(f'{opts.metric_prefix}_server_num', 'Number of servers for each user', metric_labels)
-    kernel_num_gauge = Gauge(f'{opts.metric_prefix}_kernel_num', 'Number of kernels for each user', metric_labels)
-    terminal_num_gauge = Gauge(f'{opts.metric_prefix}_terminal_num', 'Number of terminals for each user', metric_labels)
+    server_num_gauge = Gauge(f'{opts.metrics_prefix}_server_num', 'Number of servers for each user', metric_labels)
+    kernel_num_gauge = Gauge(f'{opts.metrics_prefix}_kernel_num', 'Number of kernels for each user', metric_labels)
+    terminal_num_gauge = Gauge(f'{opts.metrics_prefix}_terminal_num', 'Number of terminals for each user', metric_labels)
     
     start_http_server(int(opts.port))
     print(f'Started to monitor JupyterHub users...')
-    monitor_metrics()
+    monitor_metrics(f'http://{opts.jupyterhub_host}:{opts.jupyterhub_port}', opts.jupyterhub_token, opts.interval)
